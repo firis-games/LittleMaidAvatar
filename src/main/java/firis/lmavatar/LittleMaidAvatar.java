4769e43bf8480f31e8a62631e87a8273a54edb11
firis <firis.games.lapismoonlight@gmail.com>
2020-12-25 22:59:04 +0900

    ver1.2.2

--- src/main/java/firis/lmavatar/LittleMaidAvatar.java
+++ src/main/java/firis/lmavatar/LittleMaidAvatar.java
@@ -56,9 +56,9 @@ public class LittleMaidAvatar {
 
     public static final String MODID = "lmavatar";
     public static final String NAME = "LittleMaidAvatar";
-    public static final String VERSION = "1.2.1";
+    public static final String VERSION = "1.2.2";
     public static final String MOD_DEPENDENCIES = "required-after:forge@[1.12.2-14.23.5.2768,);"
-    		+ "required-after:lmlibrary@[1.1.3,);";
+    		+ "required-after:lmlibrary@[1.1.5,);";
     public static final String MOD_ACCEPTED_MINECRAFT_VERSIONS = "[1.12.2]";
     
     @Instance(MODID)
